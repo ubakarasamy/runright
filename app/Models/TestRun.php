a6c5e28be36056967b727731ffc2f129f267365d
ubakarasamy <smuba4@gmail.com>
2025-12-31 01:46:05 +0530

    ch4

--- app/Models/TestRun.php
+++ app/Models/TestRun.php
@@ -7,13 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class TestRun extends Model
 {
-    protected $fillable = [
+	protected $fillable = [
         'company_id',
         'project_id',
         'source',
         'status',
         'result',
+        'started_at',
+        'finished_at',
         'duration_ms',
+        'error_message',
+        'logs',
+    ];
+
+    protected $casts = [
+        'logs' => 'array',
+        'started_at' => 'datetime',
+        'finished_at' => 'datetime',
     ];
 }
 
